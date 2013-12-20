7fecc9e0ff2d2494c7a1773217cc5b83d6d144ab
kudkudak <staszek.jastrzebski@gmail.com>
2013-12-20 09:37:40 +0100

    Added maze gen, reorganized code

--- python_simulator/maze_gen.py
+++ python_simulator/maze_gen.py
@@ -2,6 +2,8 @@
 
 from defines import *
 from optparse import OptionParser
+
+
 def create_parser():
     """ Configure options and return parser object """
     parser = OptionParser()
@@ -57,11 +59,201 @@ def load_map(file_path):
 
     return grid, params
 
-if __name__ == "__main__":
-    (options, args) = create_parser().parse_args()
-    grid, metadata = load_map("maps/1.map")
-    print grid
 
-    pass
+import random
+import sys
 
 
+# constants and help for list access
+BOTTOMWALL = 0
+RIGHTWALL = 1
+VISITED = 2
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+
+# maze class definition
+class Maze:
+    #object constructor
+    def __init__(self, rows, cols):
+
+        # set maze size, walls and visited values
+        self.rows = rows
+        self.cols = cols
+        self.maze = [[[True, True, False] for j in range(cols)] for i in range(rows)]
+
+        # set current position, start and end point
+        # start and end points are used to solve the maze
+        # currrow is used to generate the maze
+        self.startrow = random.randrange(rows)
+        self.startcol = random.randrange(cols)
+
+        self.endrow = random.randrange(rows)
+        self.endcol = random.randrange(cols)
+
+        currrow = self.startrow
+        currcol = self.startcol
+
+        # The searh can be quite deep
+        if rows * cols > sys.getrecursionlimit():
+            sys.setrecursionlimit(rows * cols + 10)
+
+        # generate the maze with depth-first algorithm
+        self._gen_maze(currrow, currcol)
+
+        # number matrix for solving
+        self.numtable = [[-1 for j in range(cols)] for i in range(rows)]
+
+        #solution path
+        self.solutionpath = []
+
+    #-----------------------------------------------------------------------------
+
+    # returns the maze in ascii characters for printing on terminal
+    def __str__(self):
+
+        # the upper wall first
+        outtable = '.' + self.cols * '_.' + '\n'
+
+        for i in range(self.rows):
+            outtable += '|'
+
+            for j in range(self.cols):
+                if self.maze[i][j][BOTTOMWALL]:
+                    outtable += '_'
+                else:
+                    outtable += ' '
+                if self.maze[i][j][RIGHTWALL]:
+                    outtable += '|'
+                else:
+                    outtable += '.'
+
+            outtable += '\n'
+
+        outtable += 'Start Point   : (' + str(self.startrow) + ',' + str(self.startcol) + ')\n'
+        outtable += 'End Point     : (' + str(self.endrow) + ',' + str(self.endcol) + ')\n'
+
+        return outtable
+
+    #------------------------------------------------------------------------------
+
+    # get a list with posible directions from the current position
+    def _get_dirs(self, r, c):
+        dirlist = []
+
+        # check limits
+        if r - 1 >= 0: dirlist.append(UP)
+        if r + 1 <= self.rows - 1: dirlist.append(DOWN)
+        if c - 1 >= 0: dirlist.append(LEFT)
+        if c + 1 <= self.cols - 1: dirlist.append(RIGHT)
+
+        return dirlist
+
+    #------------------------------------------------------------------------------
+
+    # generates the maze with depth-first algorithm
+    def _gen_maze(self, r, c, d=None):
+        maze = self.maze
+
+        # knock down the wall between actual and previous position
+        maze[r][c][VISITED] = True
+        if d == UP:
+            maze[r][c][BOTTOMWALL] = False
+        elif d == DOWN:
+            maze[r - 1][c][BOTTOMWALL] = False
+        elif d == RIGHT:
+            maze[r][c - 1][RIGHTWALL] = False
+        elif d == LEFT:
+            maze[r][c][RIGHTWALL] = False
+
+        # get the next no visited directions to move
+        dirs = self._get_dirs(r, c)
+
+        # random reorder directions
+        for i in range(len(dirs)):
+            j = random.randrange(len(dirs))
+            dirs[i], dirs[j] = dirs[j], dirs[i]
+
+        # make recursive call if the target cell is not visited
+        for d in dirs:
+            if d == UP:
+                if not maze[r - 1][c][VISITED]:
+                    self._gen_maze(r - 1, c, UP)
+            elif d == DOWN:
+                if not maze[r + 1][c][VISITED]:
+                    self._gen_maze(r + 1, c, DOWN)
+            elif d == RIGHT:
+                if not maze[r][c + 1][VISITED]:
+                    self._gen_maze(r, c + 1, RIGHT)
+            elif d == LEFT:
+                if not maze[r][c - 1][VISITED]:
+                    self._gen_maze(r, c - 1, LEFT)
+
+    #------------------------------------------------------------------------------
+
+    # solve the maze by filling it with numbers(algorithm name?)
+    def _solve_maze_aux(self, r, c, n):
+        maze = self.maze
+        numtable = self.numtable
+        numtable[r][c] = n
+
+        # check if the end has been reached
+        if (r, c) != (self.endrow, self.endcol):
+            directions = self._get_dirs(r, c)
+
+            # recursive calls only if there is no wall between cells and
+            # targel cell is not marked (=-1)
+            for d in directions:
+                if d == UP and not maze[r - 1][c][BOTTOMWALL] and numtable[r - 1][c] == -1:
+                    self._solve_maze_aux(r - 1, c, n + 1)
+                elif d == DOWN and not maze[r][c][BOTTOMWALL] and numtable[r + 1][c] == -1:
+                    self._solve_maze_aux(r + 1, c, n + 1)
+                elif d == RIGHT and not maze[r][c][RIGHTWALL] and numtable[r][c + 1] == -1:
+                    self._solve_maze_aux(r, c + 1, n + 1)
+                elif d == LEFT and not maze[r][c - 1][RIGHTWALL] and numtable[r][c - 1] == -1:
+                    self._solve_maze_aux(r, c - 1, n + 1)
+
+    #------------------------------------------------------------------------------
+
+    # get the solution path
+    def _get_solution_path(self):
+        actrow = self.endrow
+        actcol = self.endcol
+        startrow = self.startrow
+        startcol = self.startcol
+        path = []
+        numtable = self.numtable
+        path = self.solutionpath
+
+        while (actrow, actcol) != (startrow, startcol):
+            path.append((actrow, actcol))
+            directions = self._get_dirs(actrow, actcol)
+            for d in directions:
+                if d == UP:
+                    if numtable[actrow][actcol] - 1 == numtable[actrow - 1][actcol]:
+                        actrow -= 1
+                        break
+                elif d == DOWN:
+                    if numtable[actrow][actcol] - 1 == numtable[actrow + 1][actcol]:
+                        actrow += 1
+                        break
+                elif d == LEFT:
+                    if numtable[actrow][actcol] - 1 == numtable[actrow][actcol - 1]:
+                        actcol -= 1
+                        break
+                elif d == RIGHT:
+                    if numtable[actrow][actcol] - 1 == numtable[actrow][actcol + 1]:
+                        actcol += 1
+                        break
+
+        path.append((actrow, actcol))
+        path.reverse()
+
+    #------------------------------------------------------------------------------
+
+    # solve the maze
+    def solve_maze(self):
+        self._solve_maze_aux(self.startrow, self.startcol, 0)
+        self._get_solution_path()
---
+++ python_simulator/simulator.py
@@ -0,0 +1,275 @@
+from visualisation import RenderToSVG, Save
+from defines import *
+from robot_controller import *
+from robot import Robot
+from maze_gen import load_map
+from math import (
+  pi, sqrt, hypot, sin, cos, tan, asin, acos, atan, atan2, radians, degrees,
+  floor, ceil, exp
+)
+import numpy as np
+import random
+from utils import logger
+
+
+class KrakrobotSimulator(object):
+    COLLISION_THRESHOLD = 50
+
+    def __init__(self,  map, init_position, steering_noise=0.01, sonar_noise = 0.1, distance_noise=0.001,
+                 measurement_noise=0.2, time_limit = 50,
+                 speed = 5.0,
+                 turning_speed = 4*pi,
+                 execution_time_limit = 10.0,
+                 simulation_dt = 0.1,
+                 frame_dt = 0.3,
+                 collision_threshold = 50
+                 ):
+        """
+            Initialize KrakrobotSimulator object
+            @param steering_noise - variance of steering in move
+            @param distance_noise - variance of distance in move
+            @param measurement_noise - variance of measurement (GPS??)
+            @param map - map for the robot simulator representing the maze or file to map
+            @param init_position - starting position of the Robot (can be moved to map class) [x,y,heading]
+
+            @param speed - distance travelled by one move action (cannot be bigger than 0.5, or he could traverse the walls)
+            @param execution_time_limit - limit in ms for whole robot execution (also with init)
+            @param collision_threshold - maximum number of collisions after which robot is destroyed
+
+            @param frame_dt - save frame every dt
+
+
+        """
+
+        if type(map) is str:
+            grid, metadata  = load_map(map)
+            self.map_title = metadata["title"]
+            self.grid = grid
+        else:
+            self.grid = map
+            self.map_title = ""
+
+        print self.grid
+
+        self.collision_threshold = collision_threshold
+
+        self.init_position = tuple(init_position)
+
+        self.speed = speed
+        self.turning_speed = turning_speed
+        self.simulation_dt = simulation_dt
+        self.frame_dt = frame_dt
+
+        self.sonar_time = 0.01
+        self.gps_time = 1.0
+        self.light_sensor_time = 0.01
+
+        self.tick_move = 0.01
+        self.tick_rotate = 0.07
+
+        self.execution_time_limit = execution_time_limit
+
+        self.goal_threshold = 0.5 # When to declare goal reach
+
+        self.sonar_noise = sonar_noise
+        self.distance_noise    = distance_noise
+        self.measurement_noise = measurement_noise
+        self.steering_noise   = steering_noise
+
+        #TODO: Move adequate actions from __init__() to reset()
+        self.reset()
+
+        self.time_limit = time_limit
+
+
+        self.N = len(self.grid)
+        self.M = len(self.grid[0])
+
+        for i in xrange(self.N):
+            for j in xrange(self.M):
+                if self.grid[i][j] == MAP_GOAL:
+                    self.goal = (i,j)
+
+
+    def create_visualisation_descriptor(self, robot):
+        """
+            @returns Descriptor that is sufficient to visualize current frame
+        """
+        data = {}
+        data['GoalThreshold'] = self.goal_threshold
+        data['Sparks'] = []# ommiting errors list(self.collisions)
+        data['ActualPath'] = list(self.robot_path)
+        data['ActualPosition'] = [robot.x, robot.y]
+        data['ActualOrientation'] = robot.orientation
+        data['Map'] = self.grid
+        data['StartPos'] = self.init_position
+        data['GoalPos'] = self.goal
+        data['GoalAchieved'] = self.goal_achieved
+        return data
+
+
+    def get_visualisation_descriptor(self, i):
+        """
+            @returns i-th frame descriptor
+        """
+        return self.frames[i]
+
+    def check_goal(self, robot):
+        """ Checks if goal is within threshold distance"""
+        dist = sqrt((float(self.goal[0]) - robot.x) ** 2 + (float(self.goal[1]) - robot.y) ** 2)
+        return dist < self.goal_threshold
+
+    def get_frames_count(self):
+        return self.frames_count
+
+    #TODO: test
+    def reset(self):
+        """ Reset state of the KrakrobotSimulator """
+        self.robot_path = []
+        self.collisions = []
+        self.goal_achieved = False
+        self.robot_timer = 0.0
+        self.frames = []
+        self.frames_count = 0
+        self.finished = False
+
+
+
+
+    def run(self, robot_controller_class):
+        """ Runs simulations by quering the robot """
+        self.reset()
+
+        # Initialize robot object
+        robot = Robot(self.speed, self.turning_speed, self.gps_time, self.sonar_time, self.tick_move, self.tick_rotate)
+        robot.set(self.init_position[0], self.init_position[1], self.init_position[2])
+        robot.set_noise(self.steering_noise,
+                        self.distance_noise,
+                        self.measurement_noise,
+                        self.sonar_noise)
+
+        # Initialize robot controller object given by contestant
+        robot_controller = robot_controller_class()
+        robot_controller.init(self.init_position, robot.steering_noise
+            ,robot.distance_noise, robot.measurement_noise, self.speed, self.turning_speed)
+
+
+        self.robot_path.append((robot.x, robot.y))
+        collision_counter = 0 # We have maximum collision allowed
+
+
+        frame_time_left = self.simulation_dt
+        frame_count = 0
+        current_command = None
+        try:
+            while not self.check_goal(robot) and not robot.time_elapsed >= self.time_limit:
+                #logger.info((robot.x, robot.y))
+
+                if frame_time_left > self.frame_dt:
+                    ### Save frame <=> last command took long ###
+
+                    self.frames.append(self.create_visualisation_descriptor(robot))
+                    self.frames_count += 1
+                    frame_time_left -= self.frame_dt
+
+                elif current_command is not None:
+                    ### Process current command ###
+
+                    if current_command[0] == TURN:
+
+                        robot = robot.turn(1) if current_command[1] > 0 else robot.turn(-1)
+                        if current_command[1] > 1: current_command = [current_command[0], current_command[1] - 1]
+                        elif current_command[1] < 1: current_command = [current_command[0], current_command[1] + 1]
+                        else: current_command = None
+
+                        frame_time_left += self.tick_rotate / self.turning_speed
+
+
+                    elif current_command[0] == MOVE:
+                        robot_proposed = robot.move(1)
+
+
+
+                        if not robot_proposed.check_collision(self.grid):
+                            collision_counter += 1
+                            self.collisions.append((robot_proposed.x, robot_proposed.y))
+                            logger.error("##Collision##")
+                            if collision_counter >= KrakrobotSimulator.COLLISION_THRESHOLD:
+                                raise KrakrobotException\
+                                        ("The robot has been destroyed by wall. Sorry! We miss WALLE already..")
+                        else:
+                            robot = robot_proposed
+                            self.robot_path.append((robot.x, robot.y))
+                            if current_command[1] > 1: current_command = [current_command[0], current_command[1] - 1]
+                            else: current_command = None
+                            frame_time_left += self.tick_move / self.speed
+
+                else:
+                    ### Get current command ###
+
+                    command = None
+                    try:
+                        command = list(robot_controller.act())
+                    except Exception, e:
+                        logger.error("Robot controller failed with exception " + str(e))
+                        break
+                    logger.info("Received command "+str(command))
+                    logger.info("Robot timer "+str(robot.time_elapsed))
+                    if not command or len(command) == 0:
+                        raise KrakrobotException("No command passed, or zero length command passed")
+
+                    # Dispatch command
+                    if command[0] == SENSE_GPS:
+                        robot_controller.on_sense_gps(robot.sense_gps())
+                        frame_time_left += self.gps_time
+                    elif command[0] == SENSE_SONAR:
+                        w = robot.sense_sonar(self.grid)
+                        logger.info("Sensed sonar : "+str(w))
+                        robot_controller.on_sense_sonar(w)
+                        frame_time_left += self.sonar_time
+                    elif command[0] == SENSE_FIELD:
+                        w = robot.sense_field(self.grid)
+                        if w == MAP_WHITE or w == MAP_WALL or w == MAP_GOAL:
+                            robot_controller.on_sense_field(w, 0)
+                        else:
+                            robot_controller.on_sense_field(w[0], w[1])
+
+                        frame_time_left += self.light_sensor_time
+                    elif command[0] == TURN:
+                        if len(command) <= 1 or len(command) > 2:
+                            raise KrakrobotException("Wrong command length")
+                        current_command = command
+                        # Turn robot
+                        #robot = robot.turn(command[1])
+
+                    elif command[0] == MOVE:
+                        if len(command) <= 1 or len(command) > 2:
+                            raise KrakrobotException("Wrong command length")
+
+                        if command[1] < 0:
+                            raise KrakrobotException("Not allowed negative distance")
+                        # Move robot
+
+                        current_command = command
+
+
+
+
+        except Exception, e:
+            logger.error("Simulation failed with exception " +str(e)+ " after " +str(robot.time_elapsed)+ " time")
+
+
+        while frame_time_left > self.frame_dt:
+            ### Save frame <=> last command took long ###
+            self.frames.append(self.create_visualisation_descriptor(robot))
+            self.frames_count += 1
+            frame_time_left -= self.frame_dt
+
+        # Simulation process finished
+        self.finished = True
+        logger.info("Simulation ended after "+str(robot.time_elapsed)+ " seconds with goal reached = "+str(self.check_goal(robot)))
+        self.goal_achieved = self.check_goal(robot)
+
+
+
+
